38241b22996729f4d381686eb1a59b14f2d92f9f
Miyagawa Kaito (宮川 海斗) <Kaito.Miyagawa@jp.ricoh.com>
2022-08-26 10:49:04 +0900

    add recording and modifying local sdp to use h264

--- src/content/peerconnection/h264-record-bug/js/main.js
+++ src/content/peerconnection/h264-record-bug/js/main.js
@@ -8,6 +8,74 @@
 
 'use strict';
 
+let mediaRecorder;
+let recordedBlobs;
+const codecPreferences = document.querySelector('#codecPreferences');
+
+let offer;
+const localSdpTextArea = document.querySelector('#localSdpTextArea');
+const setLocalSdpButton = document.querySelector('#setLocalSdpButton');
+setLocalSdpButton.addEventListener('click', async () => {
+  setLocalSdpButton.disabled = true;
+  await setLocalSdpText();
+})
+
+const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+  }
+});
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+  const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.webm';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+});
+
+const playButton = document.querySelector('button#play');
+playButton.addEventListener('click', () => {
+  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
+  const superBuffer = new Blob(recordedBlobs, {type: mimeType});
+  recordedVideo.src = null;
+  recordedVideo.srcObject = null;
+  recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  recordedVideo.controls = true;
+  recordedVideo.play();
+});
+
+function handleDataAvailable(event) {
+  console.log('handleDataAvailable', event);
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
+  }
+}
+
+function getSupportedMimeTypes() {
+  const possibleTypes = [
+    'video/webm;codecs=vp9,opus',
+    'video/webm;codecs=vp8,opus',
+    'video/webm;codecs=h264,opus',
+    'video/mp4;codecs=h264,aac',
+  ];
+  return possibleTypes.filter(mimeType => {
+    return MediaRecorder.isTypeSupported(mimeType);
+  });
+}
+
 const startButton = document.getElementById('startButton');
 const callButton = document.getElementById('callButton');
 const hangupButton = document.getElementById('hangupButton');
@@ -68,6 +136,13 @@ async function start() {
   } catch (e) {
     alert(`getUserMedia() error: ${e.name}`);
   }
+  getSupportedMimeTypes().forEach(mimeType => {
+    const option = document.createElement('option');
+    option.value = mimeType;
+    option.innerText = option.value;
+    codecPreferences.appendChild(option);
+  });
+  codecPreferences.disabled = false;
 }
 
 async function call() {
@@ -100,11 +175,56 @@ async function call() {
 
   try {
     console.log('pc1 createOffer start');
-    const offer = await pc1.createOffer(offerOptions);
+    offer = await pc1.createOffer(offerOptions);
+    localSdpTextArea.value = offer.sdp;
+    setLocalSdpButton.disabled = false;
+  } catch (e) {
+    onCreateSessionDescriptionError(e);
+  }
+}
+
+async function setLocalSdpText() {
+  offer.sdp = localSdpTextArea.value;
+  try {
     await onCreateOfferSuccess(offer);
   } catch (e) {
     onCreateSessionDescriptionError(e);
   }
+}
+
+function startRecording() {
+  recordedBlobs = [];
+  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
+  const options = {mimeType};
+
+  try {
+    mediaRecorder = new MediaRecorder(remoteVideo.srcObject, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  codecPreferences.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+  recordButton.textContent = 'Start Recording';
+  playButton.disabled = false;
+  downloadButton.disabled = false;
+  codecPreferences.disabled = false;
 }
 
 function onCreateSessionDescriptionError(error) {
@@ -158,6 +278,7 @@ function gotRemoteStream(e) {
     remoteVideo.srcObject = e.streams[0];
     console.log('pc2 received remote stream');
   }
+  recordButton.disabled = false;
 }
 
 async function onCreateAnswerSuccess(desc) {
